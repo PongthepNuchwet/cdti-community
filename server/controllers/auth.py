2c927772d0c8530316f5d636cf3aa0aa7fb9989b
PongthepNuchwet <jt061220@gmail.com>
2022-02-25 21:45:06 +0700

    git reset ada9d41

--- server/controllers/auth.py
+++ server/controllers/auth.py
@@ -6,8 +6,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import os
 
+
 from ..models import Users
-from .. import db
+from .. import db ,storage
 
 def friend_recommend_interrupt(socketio,email):
         user = [
@@ -60,8 +61,13 @@ def  Auth(socketio):
         if request.method == "POST":
             f = request.files.get("file")
             newName = secure_filename(f.filename)
-            profilePath = f"/static/profile/{newName}"
-            f.save(os.path.join("server/static/profile/", newName))
+            # profilePath = f"/static/profile/{newName}"
+            # f.save(os.path.join("server/static/profile/", newName))
+
+            storage.child("profile/{}".format(newName)).put(f)
+            afterUpload = storage.child("profile/{}".format(newName)).get_url()
+            print(afterUpload)
+
             email = request.form.get("email")
             fullName = request.form.get("fullName")
             password1 = request.form.get("password1")
@@ -81,7 +87,7 @@ def  Auth(socketio):
                 flash("Password must be at least 7 characters.", category="error")
             else:
                 new_user = Users(
-                    profile=profilePath,
+                    profile=url,
                     email=email,
                     fullName=fullName,
                     password=generate_password_hash(password1, method="sha256"),
